9378cd9eb25ecc167dd64dfd7a4616052a3a7496
Harsh Mittal <harshmittal1750@gmail.com>
2022-09-09 21:29:15 +0530

    basic UI

--- src/Components/Screens/Home.tsx
+++ src/Components/Screens/Home.tsx
@@ -1,5 +1,14 @@
 import React from "react";
-import "../../Assets/chai-garam.png";
+
+import chaiGaramImg from "../../Assets/chai-garam.png";
+import crunchyBurgerImg from "../../Assets/crunchy-burger.png";
+import desiSwagImg from "../../Assets/desi-swag.png";
+import dosaPlaza from "../../Assets/dosa-plaza.png";
+import hmCafe from "../../Assets/hmcafe.png";
+import mym from "../../Assets/mym.jpg";
+import nescafe from "../../Assets/nescafe.png";
+import rollsMafia from "../../Assets/rollsMafia.png";
+import subway from "../../Assets/subway.png";
 const Home = () => {
   return (
     <div>
@@ -39,11 +48,11 @@ const Home = () => {
           </table>
         </div> */}
       </div>
-      <div className="flex flex-row">
+      <div className="flex flex-row px-12  ">
         {" "}
-        <div className="basis-1/2 p-4">
-          <div className="text-3xl ">Why on Web3?</div>
-          <div className="">
+        <div className="basis-1/2 py-4 ">
+          <div className="text-3xl text-lime-600 ">Why on Web3?</div>
+          <div className="text-cyan-500">
             The internet has unlocked unprecedented opportunities for
             collaboration and creation. Now web3 technology like open source
             protocols and decentralized blockchains give us the ability to take
@@ -53,22 +62,22 @@ const Home = () => {
         <div className="basis-1/2">
           {" "}
           <div>
-            <div className="text-2xl my-3"> Open</div>
-            Open source code meets open economies. Build resilient projects,
-            better coordination, and positive-sum outcomes.
+            <div className="text-2xl my-3 text-lime-600"> Open</div>
+            <div className="text-cyan-500"> Open source code meets open economies. Build resilient projects,
+              better coordination, and positive-sum outcomes.</div>
           </div>
           <div>
-            <div className="text-2xl my-3"> Empowering</div>
-            <div>
+            <div className="text-2xl my-3 text-lime-600"> Empowering</div>
+            <div className="text-cyan-500">
               {" "}
               Growing networks with aligned incentives towards the wellbeing of
               each participant and the system as a whole.
             </div>
           </div>
           <div>
-            <div className="text-2xl my-3"> Collaborative</div>
+            <div className="text-2xl my-3 text-lime-600"> Collaborative</div>
 
-            <div>
+            <div className="text-cyan-500">
               {" "}
               Through distributed funding and organizations, we build together
               toward our shared goals.
@@ -76,20 +85,20 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div><div className="flex flex-row">
-        <div className="basis-1/3"><img src="./Assets/chai-garam.png" /></div>
-        <div className="basis-1/3"><img src="" /></div>
-        <div className="basis-1/3"><img src="" /></div>
+      <div className=""><div className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 ">
+        <div className="hover:bg-cyan-600 "><img className=" h-64 p-1 w-full " src={chaiGaramImg} alt="img" /></div>
+        <div className="hover:bg-cyan-600 "><img className=" h-64 p-1 w-full " src={subway} alt="img" /></div>
+        <div className=" hover:bg-cyan-600"><img className=" h-64 p-1 w-full " src={rollsMafia} alt="img" /></div>
 
-      </div><div className="flex flex-row">
-          <div className="basis-1/3"><img src="" /></div>
-          <div className="basis-1/3"><img src="" /></div>
-          <div className="basis-1/3"><img src="" /></div>
+      </div><div className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 ">
+          <div className="hover:bg-cyan-600 "><img className=" h-64 p-1 w-full " src={hmCafe} alt="img" /></div>
+          <div className="hover:bg-cyan-600 "><img className=" h-64 p-1 w-full " src={nescafe} alt="img" /></div>
+          <div className="hover:bg-cyan-600 "><img className=" h-64 p-1 w-full " src={mym} alt="img" /></div>
 
-        </div><div className="flex flex-row">
-          <div className="basis-1/3"><img src="" /></div>
-          <div className="basis-1/3"><img src="" /></div>
-          <div className="basis-1/3"><img src="" /></div>
+        </div><div className=" grid grid-cols-1  md:grid-cols-3 lg:grid-cols-3 ">
+          <div className=" hover:bg-cyan-600"><img className=" h-64 p-1 w-full " src={dosaPlaza} alt="img" /></div>
+          <div className="hover:bg-cyan-600 "><img className=" h-64 p-1 w-full " src={desiSwagImg} alt="img" /></div>
+          <div className="hover:bg-cyan-600 "><img className=" h-64 p-1 w-full " src={crunchyBurgerImg} alt="img" /></div>
 
         </div></div>
     </div>
